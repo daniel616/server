101b7f4e01ea689c4464940f5a14a6de01ce5d7d
daniel616 <li.daniel616@gmail.com>
2018-06-12 23:27:35 +0800

    worlds dumbest app

--- testapp.js
+++ testapp.js
@@ -1,14 +1,27 @@
 const path = require('path');
 const express=require('express');
+const hbs=require('hbs');
 
 var app=express();
 
+app.set('view engine','hbs');
+//app.use(express.static(__dirname+'/public'));
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.get('/',(req,res)=>{
+    res.render('index.hbs');
+})
+
+app.get('/portfolio',(req,res)=>{
+    res.render('portfolio.hbs',{
+        message:'I hop you lik my footer'
+    })
+})
+
 
 const port=process.env.PORT||3000;
-const publicPath = path.join(__dirname, '/public');
 
-
-app.use(express.static(publicPath));
 
 app.listen(port, ()=>{
     console.log(`Server is up on port ${port}`);
